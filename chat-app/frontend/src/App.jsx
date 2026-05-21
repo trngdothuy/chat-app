49694e49a7a41d2d8df9a31893a2701f915cdb7a
Trang <dothuytrang2312@gmail.com>
2026-05-21 21:15:01 +0200

    Add Frontend UI

--- chat-app/frontend/src/App.jsx
+++ chat-app/frontend/src/App.jsx
@@ -4,7 +4,18 @@ function App() {
   const [username, setUsername] = useState("");
   const [savedUsername, setSavedUsername] = useState("");
 
-  function handleSubmit(e) {
+  const [messageInput, setMessageInput] = useState("");
+
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      username: "System",
+      text: "Welcome to the chat",
+      createdAt: new Date().toLocaleTimeString(),
+    },
+  ]);
+
+  function handleUserNameSubmit(e) {
     e.preventDefault();
 
     if (!username.trim()) return;
@@ -12,12 +23,29 @@ function App() {
     setSavedUsername(username);
   }
 
+  function handleMessageSubmit(e) {
+    e.preventDefault();
+
+    if (!messageInput.trim()) return;
+
+    const newMessage = {
+      id: Date.now(),
+      userName: savedUsername,
+      text: messageInput,
+      createdAt: new Date().toLocaleTimeString(),
+    };
+
+    setMessages([...messages, newMessage]);
+
+    setMessageInput("");
+  }
+
   if (!savedUsername) {
     return (
       <div>
         <h1>Join Chat</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleUserNameSubmit}>
           <input
             type="text"
             placeholder="Enter username"
@@ -35,6 +63,29 @@ function App() {
     <div>
       <h1>Chat App</h1>
       <p>Welcome {savedUsername}!</p>
+
+      <div className="message-container">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <strong>{message.username}</strong>
+
+            <p>{message.text}</p>
+
+            <small>{message.createdAt}</small>
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={handleMessageSubmit}>
+        <input
+          type="text"
+          placeholder="Type a message"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
